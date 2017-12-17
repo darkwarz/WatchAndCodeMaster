3f7388f18feabe8d56e30517a46e041c61649f06
Jabier Espinal <jabierespinal@gmail.com>
2017-12-17 08:18:19 +0000

    added for loop lesson

---
+++ ForloopLesson/index.js
@@ -0,0 +1,17 @@
+// i = 0               // Initialization
+// //Say "hey" if i < 3  // Condition
+// //Increase i by 1     // final-expression
+
+// 0 "hey"
+// 1 "hey"
+// 2 "hey"
+// 3 "hey"
+
+for (var i = 0; i < 3; i++) {
+    console.log("hey");
+
+}
+// i = i + i
+// i++
+//for (initialization; condition; final-express) {
+//    console.log("hey");
